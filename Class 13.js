a1b4884fa64e2bf72d487011de7d805430d7b6fe
Danish12387 <135580119+Danish12387@users.noreply.github.com>
2023-08-07 13:23:54 +0500

    new class files added

---
+++ Class 13.js
@@ -0,0 +1,88 @@
+// alert('Hello World')
+
+// console.log('Hello World')
+
+// var danishAll= ('Hello Wolrd')
+// var number=1000
+// var boolean=true //true false
+// var obj_ect={}
+// var array=[]
+// var undefined=undefined //null
+
+// console.log(typeof danishAll)
+// console.log(typeof number)
+// console.log(typeof boolean)
+// console.log(typeof obj_ect)
+// console.log(typeof array)
+// console.log(typeof undefined)
+
+//Hoisting
+
+// There are two things in variable
+// 1.Variable Declaration
+// 2.Assign value to Variable
+
+var username='danish';
+
+// if you want to update the variable you declared before so just put the name of variable without var keyword and updated value
+
+username=true
+
+console.log(username)
+
+username=1000
+
+console.log(username)
+
+var abc = "ashir Saleem"
+
+abc = "saleem uddin" 
+
+console.log(abc)
+
+console.log(hello)
+
+var hello="hello world"
+
+mine="thousands"
+
+console.log(mine)
+
+// three types of scopes
+// 1.Global Scope
+// 2.Funtional Scope
+// 3.Block Scope
+
+var age = 18 
+ age =19 
+ console.log(age)
+
+
+ var favcolor = "Black"
+ favcolor ="Yellow"
+console.log(favcolor)
+
+
+
+var temperature = 30 
+ temperature = 35 
+console.log(temperature)
+
+
+var balance = 0 
+
+
+console.log(balance)
+
+var numb1 = 40
+var numb2 = 10
+ 
+console.log(numb1 + numb2)
+
+// console.log(numb1 / numb2)
+
+console.log(numb1 * numb2)
+
+console.log(numb1 % numb2)
+
+console.log(++numb1)
---
+++ Class 14.js
@@ -0,0 +1,21 @@
+
+// parseFloat("20") //It converts string to numbers.
+console.log(parseFloat('12'))
+
+// +"20" //It will convert string to number.
+console.log(+'20')
+
+//Number('14') //It will also converts strings to numbers.
+console.log(Number('14'))
+
+// let userAge = prompt('Your Age')
+
+// let nextYearsAge = userAge
+
+// alert('You will be ' + (++userAge) +  ' years old next year.')
+
+let number=prompt('Enter Your Number')
+
+if (number % 2 === 0) alert('Its even number.')
+
+else alert('It is odd.')
