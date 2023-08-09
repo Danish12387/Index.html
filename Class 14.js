ee6eacc2f31ae38af5b48749dbd9e7dc98561212
Danish12387 <135580119+Danish12387@users.noreply.github.com>
2023-08-09 20:09:10 +0500

    new class files added

--- Class 14.js
+++ Class 14.js
@@ -1,5 +1,5 @@
 
-// parseFloat("20") //It converts string to numbers.
+// parseFloat("12") //It converts string to numbers.
 console.log(parseFloat('12'))
 
 // +"20" //It will convert string to number.
@@ -9,13 +9,75 @@ console.log(+'20')
 console.log(Number('14'))
 
 // let userAge = prompt('Your Age')
-
 // let nextYearsAge = userAge
-
 // alert('You will be ' + (++userAge) +  ' years old next year.')
 
-let number=prompt('Enter Your Number')
+// let number=prompt('Enter Your Number')
+// if (number % 2 === 0) {alert('Its even number.')}
+// else {alert('It is odd.')}
 
-if (number % 2 === 0) alert('Its even number.')
+// let userAge=prompt('Your Age')
+// if (userAge>=18){alert("You are an Adult.")}
+// else{alert("You are a minor.")}
 
-else alert('It is odd.')
+// let name=prompt('Your Name')
+// if ( name ==='Jhon'){alert("Hello, Jhon!")}
+// else{alert("Hello, Stranger!")}
+
+// let area=prompt('Enter Length')
+// let area2=prompt('Enter Width')
+// {alert ("Your area is " + area*area2+'.')}
+
+// let userAge=prompt('Enter You Age')
+// if(userAge >= 13 && userAge <= 19){alert('You are a teenager.')}
+// else{alert('You are not a teenager.')}
+
+// let height=prompt('Enter Height')
+// let radius=prompt('Enter Radius')
+// {alert('The Volume is ' + height * radius * radius * Math.PI)}
+
+// let number=+(prompt('Enter a Number.'))
+// if(number>0){alert('The number you entered is positive.')}
+// else if(number<0){alert('The number you entered is negative.')}
+// else if(number===0){alert('The number you entered is zero.')}
+// else{alert("Invalid number.")}
+
+// var grade=prompt('Enter your grade (A, B, C, or D).')
+// var uppercaseGrade = grade.toUpperCase()
+// if (uppercaseGrade ==="A" || uppercaseGrade ==="B" || uppercaseGrade ==="C" || uppercaseGrade ==="D"){alert('Congratulations, You have Passed!.')}
+// else if (uppercaseGrade==="F"){alert('Sorry, you failed.')}
+// else {alert('Invalid Grade.')}
+
+// var number1=+(prompt('Enter first number.'))
+// var number2=+(prompt('Enter second number.'))
+// var number3=+(prompt('Enter third number.'))
+// var maximum=Math.max(number1, number2, number3)
+// var minimum=Math.min(number1, number2, number3)
+// {alert('Maximum: ' + maximum + ' , ' + 'Minimum: ' + minimum)}
+
+// var price=prompt('Enter item price:')
+// var discount=prompt('Enter the discount percentage:')
+// var discountprice=price-(price*discount/100)
+// {alert("The Discounted Price is " + discountprice + '.')}
+
+// var originalPrice = parseFloat(prompt("Enter the original price:"));
+// var discountPercentage = parseFloat(prompt("Enter the discount percentage:"));
+// // Calculate the discounted price
+// var discountAmount = (discountPercentage / 100) * originalPrice;
+// var discountedPrice = originalPrice - discountAmount;
+// // Display the result
+// alert("Original Price: $" + originalPrice.toFixed(2));
+// alert("Discount Percentage: " + discountPercentage.toFixed(2) + "%");
+// alert("Discount Amount: $" + discountAmount.toFixed(2));
+// alert("Discounted Price: $" + discountedPrice.toFixed(2));
+
+// var userAge=prompt('Enter your age.')
+// if (userAge<13){alert('You are a child.')}
+// else if(userAge>=13 && userAge<=19){alert("You are a teenager.")}
+// else if(userAge>=20){alert('You are an adult.')}
+// else{alert('Invalid age.')}
+
+// var username=prompt('Enter your name.')
+// if (username.charAt(0).toLowerCase() === "a"){alert('Your name starts with letter A!')}
+// else {alert("Your name doesn't start with letter A!")}
+
