38f83a0832094f263d7d810be8151daef7479ed5
Danish12387 <135580119+Danish12387@users.noreply.github.com>
2023-08-09 11:07:31 +0500

    new added

---
+++ Class 15.js
@@ -0,0 +1,18 @@
+// let userage = prompt("Enter your age")
+// if(userage >=18 ){alert("you are an adult.")}
+// else if(userage <18){alert("you are minor.")} 
+// else{alert("Invalid age.")}
+
+let userpercentage =prompt("your percentage.")
+
+if (userpercentage >90 && userpercentage <=100){
+alert("your grade A+.")}
+else if(userpercentage >80 && userpercentage <=90){
+    alert("your grade is A.")
+}
+else if(userpercentage >=70 && userpercentage <=80){
+    alert("your grade is B.")
+}
+else{
+    alert("you are fail gand marao")
+}
